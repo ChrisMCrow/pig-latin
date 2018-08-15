422a67fdf3f9f8bcb63dcedb022923fb7f1f46d1
Chris Crow and Connor McCarthy <chris@>
2018-08-15 14:41:11 -0700

    show progress with 6th spec

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@ var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q
 
 var translator = function(userInput) {
   if (consonants.includes(userInput[0])) {
-    for (index = 0; index < userInput.length; index++); {
-      var searchTerm = vowels[index];
-      var indexOfFirst = userInput.indexOf(searchTerm);
+    for (var index = -1; userInput.indexOf(vowels[index]) < 0; index++) {
+      var vowelLocation = index + 1;
+      var indexOfFirst = userInput.indexOf(vowels[vowelLocation]);
     }
-    if (indexOfFirst > 2) {
+    alert(indexOfFirst);
+
+    if (indexOfFirst > 1) {
       var slicedInput = userInput.slice(indexOfFirst)
-      return slicedInput + userInput[0,indexOfFirst] + "ay";
+      return slicedInput + userInput.slice(0, indexOfFirst) + "ay";
     } else {
       var slicedInput = userInput.slice(1);
       return slicedInput + userInput[0] + "ay";
