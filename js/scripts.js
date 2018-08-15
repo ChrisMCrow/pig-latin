6eca7a2a35018301009473d5e6cc8622baafce3d
Chris Crow and Connor McCarthy <connor@>
2018-08-15 11:30:19 -0700

    pass 4th spec

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ var vowels = ["a", "e", "i", "o", "u", "y"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w",  "x", "z"];
 
 var translator = function(userInput) {
-  if (vowels.includes(userInput[0])){
+  if (userInput[0] === "y"){
+    userInput = userInput.slice(1);
+    return userInput + 'yay'
+  } else if (vowels.includes(userInput[0])){
     return userInput + 'way';
   } else if (vowels.includes(userInput)){
     return userInput + 'way';
