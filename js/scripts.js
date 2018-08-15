f90bd1dc8ae3e6a756734c15fed974a9d00472b6
Chris Crow and Connor McCarthy <connor@>
2018-08-15 15:58:43 -0700

    pass 10th spec. All specs pass.

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,43 @@ var vowels = ["a", "e", "i", "o", "u", "y"];
 var vowelRegex = /a|e|i|o|u|y/
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w",  "x", "z"];
 
-var translator = function(userInput) {
-  userInput = userInput.toLowerCase();
-  if (consonants.includes(userInput[0])) {
-    var indexOfFirstV = userInput.search(vowelRegex);
-    var indexOfFirstQ = userInput.search("q");
-    if (indexOfFirstQ < indexOfFirstV && indexOfFirstQ > -1) {
-      var qSlice = userInput.slice(indexOfFirstQ + 2);
-      return qSlice + userInput.slice(0, indexOfFirstV + 1) + "ay";
-    } else if (indexOfFirstV > 1) {
-      var slicedInput = userInput.slice(indexOfFirstV)
-      return slicedInput + userInput.slice(0, indexOfFirstV) + "ay";
+var translator = function(initialInput) {
+  var initialInputLC = initialInput.toLowerCase();
+  var userInputArray = initialInputLC.split(" ");
+  var pigLatinArray = userInputArray.map(function(userInput) {
+    if (consonants.includes(userInput[0])) {
+      var indexOfFirstV = userInput.search(vowelRegex);
+      var indexOfFirstQ = userInput.search("q");
+      if (indexOfFirstQ < indexOfFirstV && indexOfFirstQ > -1) {
+        var qSlice = userInput.slice(indexOfFirstQ + 2);
+        return qSlice + userInput.slice(0, indexOfFirstV + 1) + "ay";
+      } else if (indexOfFirstV > 1) {
+        var slicedInput = userInput.slice(indexOfFirstV)
+        return slicedInput + userInput.slice(0, indexOfFirstV) + "ay";
+      } else {
+        var slicedInput = userInput.slice(1);
+        return slicedInput + userInput[0] + "ay";
+      }
+    } else if (userInput[0] === "y"){
+      userInput = userInput.slice(1);
+      return userInput + 'yay'
+    } else if (vowels.includes(userInput[0])){
+      return userInput + 'way';
+    } else if (vowels.includes(userInput)){
+      return userInput + 'way';
     } else {
-      var slicedInput = userInput.slice(1);
-      return slicedInput + userInput[0] + "ay";
+      return userInput;
     }
-  } else if (userInput[0] === "y"){
-    userInput = userInput.slice(1);
-    return userInput + 'yay'
-  } else if (vowels.includes(userInput[0])){
-    return userInput + 'way';
-  } else if (vowels.includes(userInput)){
-    return userInput + 'way';
-  } else {
-    return userInput;
-  }
-}
-
+  });
+  return pigLatinArray.join(" ");
+};
 
 //front-end
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#userInput").val();
-    var output = translator(userInput);
+    var initialInput = $("#userInput").val();
+    var output = translator(initialInput);
     $("#output").text(output)
   });
 });
